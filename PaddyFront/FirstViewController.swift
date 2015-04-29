22d50a7f9b5087923206ccc6828dfc8432d8aebb
K.Mochizuki <mujen.i@gmail.com>
2015-04-29 18:40:21 +0900

    部品配置

---
+++ PaddyFront/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PaddyFront
+//
+//  Created by K.Mochizuki on 2015/04/29.
+//  Copyright (c) 2015年 K.Mochizuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
